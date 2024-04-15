a1b08ee160c0f3d97a2ead8b905ce309886cc31c
maxandul <149802114+maxandul@users.noreply.github.com>
2024-04-15 20:49:13 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,5 @@
 // Firebase-Initialisierung
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCST5U-amIwPcdTFAglfD8ddMeSa5BJF_Y",
-    authDomain: "gourmen.firebaseapp.com",
-    databaseURL: "https://gourmen-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "gourmen",
-    storageBucket: "gourmen.appspot.com",
-    messagingSenderId: "50818690491",
-    appId: "1:50818690491:web:8bfa42ec10660b5efba89e",
-    measurementId: "G-Q0GM6VPF6Q"
-  };
-firebase.initializeApp(firebaseConfig);
 
 // Anmeldeformular abfangen
 const loginForm = document.querySelector('#login-form');
